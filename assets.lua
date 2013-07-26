21d00cfe0c348c8d55b95afabd68caac4055bfd8
Paulin Shek <paulin.shek@gmail.com>
2013-07-26 09:31:58 +0100

    added makefile and assets.lua

---
+++ assets.lua
@@ -0,0 +1,29 @@
+--Metadata
+
+IconAssets = group{}
+Icon = image{"icon.png"}
+
+
+--Menu assets
+
+MenuAssets = group{}
+
+MenuImages0 = list{ image{"assets/menu_singleplayer.png"}, image{"assets/menu_multiplayer.png"}, image{"assets/menu_stats"} }
+
+MenuImages_multi_num = list{ image{"assets/menu_multi_2.png"}, image{"assets/menu_multi_3.png"}, image{"assets/menu_multi_4.png"} }
+
+MenuImages_single = list{ image{"assets/menu_normalplay.png"}, image{"assets/menu_practise.png"} }
+MenuImages_multi = list{ image{"assets/menu_competitive.png"}, image{"assets/menu_teamwork.png"} }
+
+
+--Game assets. Operators should be in the order as defined in Cube.h
+
+GameAssets = group{}
+
+Background = image{"assets/background.png"}
+
+Gameover = image{"assets/gameover.png"}
+
+OpHighlight = list { image{"assets/operator.divide.png"}, image{"assets/operator.minus.png"}, image{"assets/operator.plus.ng"}, image{"assets/operator.times.png"}}
+Ops = list { image{"assets/plus.png"}, image{"assets/minus.png"}, image{"assets/times.png"}, image{"assets/divide.png"}}
+
