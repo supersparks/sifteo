cc24fa8c640ff39b1907adef012a083ad3b855e7
Sophia Saller <smf.saller@gmail.com>
2013-08-01 16:35:54 +0100

    no message

---
+++ src/Cubes/Question.d
@@ -0,0 +1,119 @@
+Cubes/Question.o: Cubes/Question.cpp Cubes/Question.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/syscall.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/types.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/audio.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/events.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/vram.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/asset.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/elf.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/array.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/memory.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/macros.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/group.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/limits.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/math.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/loader.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/audio.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/image.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/audio.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/cube.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/event.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/filesystem.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/metadata.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/motion.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/string.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/system.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/time.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/color.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/sprite.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/framebuffer.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0rom.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg1.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg2.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/tilebuffer.h \
+  Cubes/QuestionList.h Cubes/.././GameDrawer.h
+
+Cubes/Question.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/syscall.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/types.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/audio.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/events.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/vram.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/asset.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/elf.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/array.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/memory.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/macros.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/group.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/limits.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/math.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/loader.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/audio.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/image.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/audio.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/cube.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/event.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/filesystem.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/metadata.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/motion.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/string.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/system.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/time.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/color.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/sprite.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/framebuffer.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0rom.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg1.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg2.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/tilebuffer.h:
+
+Cubes/QuestionList.h:
+
+Cubes/.././GameDrawer.h:
---
+++ src/Modes/Rules.d
@@ -0,0 +1,149 @@
+Modes/Rules.o: Modes/Rules.cpp Modes/Rules.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/syscall.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/types.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/audio.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/events.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/vram.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/asset.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/elf.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/array.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/memory.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/macros.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/group.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/limits.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/math.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/loader.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/audio.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/image.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/audio.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/cube.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/event.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/filesystem.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/metadata.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/motion.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/string.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/system.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/time.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/color.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/sprite.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/framebuffer.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0rom.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg1.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg2.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/tilebuffer.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/types.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/util.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/public.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/states.h \
+  /Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/events.h \
+  Modes/./../GameDrawer.h Modes/./../Modes/Mode.h assets.gen.h \
+  Modes/./../Cubes/Questioner.h Modes/./../Cubes/Question.h \
+  Modes/./../Cubes/QuestionList.h Modes/./../Cubes/Result.h
+
+Modes/Rules.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/syscall.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/types.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/audio.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/events.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/vram.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/asset.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/abi/elf.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/array.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/memory.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/macros.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/group.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/limits.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/math.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/loader.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/audio.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/asset/image.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/audio.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/cube.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/event.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/filesystem.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/metadata.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/motion.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/string.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/system.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/time.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/color.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/sprite.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/framebuffer.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0rom.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg0.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg1.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/bg2.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/video/tilebuffer.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/types.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/util.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/public.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/states.h:
+
+/Users/sparxmacair2/Intern/sifteo-sdk/include/sifteo/menu/events.h:
+
+Modes/./../GameDrawer.h:
+
+Modes/./../Modes/Mode.h:
+
+assets.gen.h:
+
+Modes/./../Cubes/Questioner.h:
+
+Modes/./../Cubes/Question.h:
+
+Modes/./../Cubes/QuestionList.h:
+
+Modes/./../Cubes/Result.h:
